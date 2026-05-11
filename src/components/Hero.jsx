4ef8991f0f6908d198a6feae3bdedad81161d0a7
ariq-azmain <ariq-azmain123@gmail.com>
2026-05-11 09:25:24 +0600

    modify heading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Your AI Assistant That&nbsp;Takes&nbsp;Action with {` `}
+            &nbsp;Take&nbsp;Action with {` `}
             <span className="inline-block relative">
               Action AI{" "}
               <img
